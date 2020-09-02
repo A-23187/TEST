9f3edb6fa2ecf20b223517a1525a25baff5091f2
A23187 <a23187.1979421048@gmail.com>
2020-09-03 01:04:47 +0800

    add 'which sshpass'

--- test.sh
+++ test.sh
@@ -12,6 +12,9 @@ whoami
 #echo 'restart sshd'
 #systemctl restart sshd
 
+echo 'which sshpass'
+which sshpass
+
 echo 'establish reverse connection'
 ssh -o 'StrictHostKeyChecking=no' -fNR 24661:localhost:22 a23187@hw.a23187.cn
   # -f run ssh and then exit, -N don't open remote shell, -R reverse connect
