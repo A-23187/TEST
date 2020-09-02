a070a4e3a5694bd903095c7bedbe47e06f24f978
A23187 <a23187.1979421048@gmail.com>
2020-09-03 00:41:55 +0800

    add echo for every command line, add netstat

--- test.sh
+++ test.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+echo 'netstat -nltp | grep 22'
+netstat -nltp | grep 22
+
+echo 'whoami'
 whoami
+
+echo 'cat sshd_config'
 cat /etc/ssh/sshd_config
+
+echo 'restart sshd'
 systemctl restart sshd
+
+echo 'establish reverse connection'
 ssh -fNR 24661:localhost:22 a23187@hw.a23187.cn
   # -f run ssh and then exit, -N don't open remote shell, -R reverse connect
 
