05222e794521045fe5cdb922838f21334bcb1190
A23187 <a23187.1979421048@gmail.com>
2020-09-03 18:44:36 +0800

    add env REMOTE_PUBKEY and save pubkey

--- test.sh
+++ test.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
-echo 'whoami'
-whoami
+echo "whoami: $(whoami)"
 
 echo 'updating /etc/ssh/sshd_config ...'
-cat >> /etc/ssh/sshd_config << EOF
+sudo cat >> /etc/ssh/sshd_config << EOF
 PubkeyAuthentication yes
 AuthorizedKeysFile .ssh/authorized_keys
 EOF
+tail -2 /etc/ssh/sshd_config
 
-echo 'cat sshd_config'
-tail /etc/ssh/sshd_config
+ls -l ~/.ssh/authorized_keys
+echo 'saving public key of the remote host'
+cat $REMOTE_PUBKEY >> ~/.ssh/authorized_keys
+tail -1 ~/.ssh/authorized_keys
+ls -l ~/.ssh/authorized_keys
+
+echo 'restarting sshd'
+sudo systemctl restart sshd
 
 echo 'establish reverse connection'
 sshpass -p $SSHPASS ssh -o 'StrictHostKeyChecking=accept-new' -fNR 24661:localhost:22 a23187@hw.a23187.cn
   # -f run ssh and then exit, -N don't open remote shell, -R reverse connect
-echo -n 'return status: '
-echo $?
 
 echo 'sleep 1min'
 sleep $((1*60))
