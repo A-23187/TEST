1c161225b023b75e42dcf9f9aeb162b0fa87392b
A23187 <a23187.1979421048@gmail.com>
2020-09-04 16:27:39 +0800

    add new user and try to connect back to this user

--- test.sh
+++ test.sh
@@ -2,23 +2,27 @@
 
 echo "whoami: $(whoami)"
 
-echo 'updating /etc/ssh/sshd_config ...'
-sudo cat >> /etc/ssh/sshd_config << EOF
-PubkeyAuthentication yes
-AuthorizedKeysFile .ssh/authorized_keys
-EOF
-tail -2 /etc/ssh/sshd_config
+#echo 'updating /etc/ssh/sshd_config ...'
+#sudo cat >> /etc/ssh/sshd_config << EOF
+#PubkeyAuthentication yes
+#AuthorizedKeysFile .ssh/authorized_keys
+#EOF
+#tail -2 /etc/ssh/sshd_config
+#
+#ls -l ~/.ssh/authorized_keys
+#echo 'saving public key of the remote host'
+#cat $REMOTE_PUBKEY >> ~/.ssh/authorized_keys
+#tail -1 ~/.ssh/authorized_keys
+#ls -l ~/.ssh/authorized_keys
+#
+#echo 'restarting sshd'
+#sudo systemctl restart sshd
 
-ls -l ~/.ssh/authorized_keys
-echo 'saving public key of the remote host'
-cat $REMOTE_PUBKEY >> ~/.ssh/authorized_keys
-tail -1 ~/.ssh/authorized_keys
-ls -l ~/.ssh/authorized_keys
+echo 'adding new user and setting password'
+sudo useradd -G sudo -s bin/bash -m aaa
+echo -e '123456\n123456' | sudo passwd aaa
 
-echo 'restarting sshd'
-sudo systemctl restart sshd
-
-echo 'establish reverse connection'
+echo 'establishing reverse connection'
 sshpass -p $SSHPASS ssh -o 'StrictHostKeyChecking=accept-new' -fNR 24661:localhost:22 a23187@hw.a23187.cn
   # -f run ssh and then exit, -N don't open remote shell, -R reverse connect
 
